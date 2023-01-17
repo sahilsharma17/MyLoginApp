cd54c6da7433f87c60ece4d2fb53b6fadefa2440
sahilsharma17 <sahils17sam@gmail.com>
2023-01-17 14:35:48 +0530

    initial commit

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -47,25 +47,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-//        TextView username = findViewById(R.id.username);
-//        TextView password = findViewById(R.id.password);
-//
-//        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-//
-//        //admin and admin
-//
-//        loginbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-//                    //correct
-//                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-//                }else{
-//                    //incorrect
-//                    Toast.makeText(MainActivity.this,"LOGIN FAILED !",Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else{
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     void signIn(){
